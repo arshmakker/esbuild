f08f225bf8efe80eeeeb89caba13d779f0aa2a45
Evan Wallace <evan.exe@gmail.com>
2020-01-19 12:03:28 -0800

    parse .ts and .tsx files as .js and .jsx

--- src/esbuild/bundler/bundler.go
+++ src/esbuild/bundler/bundler.go
@@ -59,6 +59,17 @@ func parseFile(
 		ast, ok := parser.Parse(log, source, options)
 		results <- parseResult{source.Index, ast, ok}
 
+	case strings.HasSuffix(path, ".ts"):
+		options.TS.Parse = true
+		ast, ok := parser.Parse(log, source, options)
+		results <- parseResult{source.Index, ast, ok}
+
+	case strings.HasSuffix(path, ".tsx"):
+		options.TS.Parse = true
+		options.JSX.Parse = true
+		ast, ok := parser.Parse(log, source, options)
+		results <- parseResult{source.Index, ast, ok}
+
 	case strings.HasSuffix(path, ".json"):
 		expr, ok := parser.ParseJson(log, source)
 		ast := parser.ModuleExportsAST(source, expr)
